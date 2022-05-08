1aecb180fe399ff485c13017f4aa0ae7489ac716
Sky-to-piece <74625401+Sky-to-piece@users.noreply.github.com>
2022-05-09 00:49:50 +0300

    Update Policlinic.sql

--- Policlinic.sql
+++ Policlinic.sql
@@ -4,38 +4,38 @@ USE Polyclinic;
 
 CREATE TABLE DOCTORS(
 					[ID] INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
-					[Surname]		VARCHAR(255) NOT NULL,
-					[Name]			VARCHAR(255) NOT NULL,
+					[Surname]	VARCHAR(255) NOT NULL,
+					[Name]		VARCHAR(255) NOT NULL,
 					[Patronymic]	VARCHAR(255) NOT NULL,
 					[Specialization] VARCHAR(255) NOT NULL,
 					);
 CREATE TABLE PATIENT(
 					[ID] INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
-					[Surname]		VARCHAR(255) NOT NULL,
-					[Name]			VARCHAR(255) NOT NULL,
+					[Surname]	VARCHAR(255) NOT NULL,
+					[Name]		VARCHAR(255) NOT NULL,
 					[Patronymic]	VARCHAR(255) NOT NULL,
-					[DateOfBirth]	DATE NOT NULL,
-					[Address]		VARCHAR(255) NOT NULL,
+					[DateOfBirth]	DATE 	NOT NULL,
+					[Address]	VARCHAR(255) NOT NULL,
 					);
 CREATE TABLE RECEPTION(
 					[ID] INT IDENTITY(1,1)	PRIMARY KEY NOT NULL,
 					[TheCostOfAdmission]	VARCHAR(255) NOT NULL,
-					[Percent]				VARCHAR(255) NOT NULL,
-					[DateOfAdmission]		DATE	NOT NULL,
-					[ID_DOCTORS]			INT		NOT NULL,
-					[ID_PATIENT]			INT		NOT NULL,
+					[Percent]		VARCHAR(255) NOT NULL,
+					[DateOfAdmission]	DATE	NOT NULL,
+					[ID_DOCTORS]		INT	NOT NULL,
+					[ID_PATIENT]		INT	NOT NULL,
 					FOREIGN KEY ([ID_DOCTORS]) REFERENCES DOCTORS([ID]), --one-to-many
 					FOREIGN KEY ([ID_PATIENT]) REFERENCES PATIENT([ID]), --one-to-many
 					);
 
 INSERT INTO DOCTORS	([Surname],	[Name],	[Patronymic],	[Specialization]) 
-VALUES				('Ivanov',	'Ivan', 'Ivanovich',	'Surgeon');
+VALUES			('Ivanov',	'Ivan', 'Ivanovich',	'Surgeon');
 
 INSERT INTO PATIENT	([Surname],		[Name],		[Patronymic],	[DateOfBirth],	[Address]) 
-VALUES				('Ivanovichev',	'Ivan',		'Ivanovich',	'28.03.2001',	'Stavropol territory, Kislovodsk, Pushkin Street, 12');
+VALUES			('Ivanovichev',		'Ivan',		'Ivanovich',	'28.03.2001',	'Stavropol territory, Kislovodsk, Pushkin Street, 12');
 
 INSERT INTO RECEPTION	([TheCostOfAdmission],	[Percent],	[DateOfAdmission],	[ID_DOCTORS],	[ID_PATIENT]) 
-VALUES					('1500',				'87%',		'2007.09.25',		'1',			'1');
+VALUES			('1500',		'87%',		'2007.09.25',		'1',		'1');
 
 SELECT	RECEPTION.DateOfAdmission AS 'Дата приёма',
 		RECEPTION.TheCostOfAdmission AS 'Стоимость приёма',
