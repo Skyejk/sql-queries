14365ffa7bf3545f4d143b1c896d9048398c54f4
Sky-to-piece <74625401+Sky-to-piece@users.noreply.github.com>
2022-06-01 22:53:53 +0300

    Addition to ReactiveSnail.sql
    При реализации появился более рациональный взгляд на базу и были внесены закомментированные изменения, которые лучше оставить для будущих времён.

--- ReactiveSnail.sql
+++ ReactiveSnail.sql
@@ -18,6 +18,11 @@ CREATE TABLE DRIVER(
 INSERT INTO DRIVER (LASTNAME, FIRSTNAME, PATRONYMIC, YEAR_OF_BIRTH, DATE_OF_EMPLOYMENT, DRIVING_EXPERIENCE, PHOTO)
 VALUES ("Ivanov", "Ivan", "Ivanovich", '1995.03.03', '2016.03.03', '2015.03.09', "/photo/IvanII.png");
 
+/*
+INSERT INTO drivers (lastname, firstname, patronymic, year_of_birth, date_of_employment, driving_experience, photo)
+VALUES ("Ivanov", "Ivan", "Ivanovich", '1995-03-03', '2016-03-03', '2015-03-09', "/photo/IvanII.png");
+*/
+
 CREATE TABLE TRANSPORT_REFERENCES(
     ID INT PRIMARY KEY AUTO_INCREMENT 	    NOT NULL,
     TYPE_TRAN_REF           VARCHAR(255)    NOT NULL,
@@ -30,6 +35,11 @@ CREATE TABLE TRANSPORT_REFERENCES(
 INSERT INTO TRANSPORT_REFERENCES (TYPE_TRAN_REF, CAR_MODEL, STATUS_TRAN_REF, DATE_OF_REGISTRATION)
 VALUES ("Passenger car", "LADA Granta", "Active", '2016.03.03');
 
+/*
+INSERT INTO transports (type_transport, car_model, status_transport, date_of_registration , date_of_de_registration)
+VALUES ("Cargo car", "Gazel'ka 3302", "Active", '2005-07-25', '2015-07-25');
+*/
+
 CREATE TABLE DRIVER_TRAN_REF(
     ID INT PRIMARY KEY AUTO_INCREMENT 	NOT NULL,
     ID_DRIVER       INT                 NOT NULL,
@@ -39,3 +49,11 @@ CREATE TABLE DRIVER_TRAN_REF(
     );
 
 INSERT INTO DRIVER_TRAN_REF (ID_DRIVER, ID_TRAN_REF) VALUES ('1', '1');
+
+/*
+INSERT INTO relation_dr_tr (id_driver, id_transport) VALUES ('5', '3');
+*//*
+ALTER TABLE relation_dr_tr ADD CONSTRAINT relation_dr_tr_FK_drivers FOREIGN KEY (id_driver) REFERENCES drivers (id) ON UPDATE NO ACTION ON DELETE NO ACTION;
+*//*
+ALTER TABLE relation_dr_tr ADD CONSTRAINT relation_dr_tr_FK_transports FOREIGN KEY (id_transport) REFERENCES transports (id) ON UPDATE NO ACTION ON DELETE NO ACTION;
+*/
