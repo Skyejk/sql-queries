f1a9d89694f90fe56d6dc8a9f86d13a96ef48fd3
Skye Jk <74625401+Skyejk@users.noreply.github.com>
2024-06-12 11:37:22 +0300

    Update spaceplaceappDB.sql
    update 12.06.24 11:37

--- spaceplaceappDB.sql
+++ spaceplaceappDB.sql
@@ -1,12 +1,42 @@
 /* use postgreSQL */
 
+CREATE TABLE Gender(
+	GenderId SERIAL NOT NULL,
+	GenderName VARCHAR(12) DEFAULT('Male') NOT NULL,
+	PRIMARY KEY (GenderId)
+);
+
+INSERT INTO Gender VALUES ('Male');
+INSERT INTO Gender VALUES ('Famale');
+
+CREATE TABLE Human(
+	HumanId SERIAL NOT NULL,
+	GenderId INTEGER DEFAULT(1) NOT NULL,
+	HumanLastName VARCHAR(128) NOT NULL,
+	HumanFirstName VARCHAR(128) NOT NULL,
+	HumanPatronymic VARCHAR(128) NULL,
+	HumanDateOfBirth DATE DEFAULT CURRENT_DATE NOT NULL,
+	PRIMARY KEY (HumanId),
+	FOREIGN KEY (GenderId) REFERENCES Gender (GenderId)
+);
+
+CREATE TABLE Employee(
+	EmployeeId SERIAL NOT NULL,
+	EmployeeName VARCHAR(48) DEFAULT('Guest') NOT NULL,
+	PRIMARY KEY (EmployeeId)
+);
+
+INSERT INTO Employee VALUES ('Guest');
+
 CREATE TABLE Users(
-	user_id SERIAL,
-	user_name varchar(128) NOT NULL,
-	user_pass varchar(128) NOT NULL,
-	user_employee varchar(128) DEFAULT('guest') NOT NULL,
-	user_gender varchar(12) NOT NULL,
-	user_date_of_birth date NOT NULL,
-	user_avatar text DEFAULT('guest.png'),
-	PRIMARY KEY (user_id)
+	UserId SERIAL NOT NULL,
+	HumanId INTEGER NOT NULL,
+	EmployeeId INTEGER DEFAULT(1) NOT NULL,
+	UserName varchar(128) NOT NULL,
+	UserPass varchar(128) NOT NULL,
+	UserMail varchar(256) NULL,
+	UserImage text DEFAULT('guest.png') NULL,
+	PRIMARY KEY (user_id),
+	FOREIGN KEY (HumanId) REFERENCES Human (HumanId),
+	FOREIGN KEY (EmployeeId) REFERENCES Employee (EmployeeId)
 );
