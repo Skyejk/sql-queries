4f20c775dc04d4ae46cc8eb6d38184442f18fad5
Skye Jk <74625401+Skyejk@users.noreply.github.com>
2024-06-12 08:45:03 +0300

    Update spaceplaceappDB.sql
    small fix

--- spaceplaceappDB.sql
+++ spaceplaceappDB.sql
@@ -1,10 +1,12 @@
-CREATE TABLE users(
-	user_id integer IDENTITY(1,1),
+/* use postgreSQL */
+
+CREATE TABLE Users(
+	user_id SERIAL,
 	user_name varchar(128) NOT NULL,
 	user_pass varchar(128) NOT NULL,
-	user_employee varchar(128) DEFAULT(guest) NOT NULL,
+	user_employee varchar(128) DEFAULT('guest') NOT NULL,
 	user_gender varchar(12) NOT NULL,
 	user_date_of_birth date NOT NULL,
-	user_avatar text,
-	PRIMARY KEY (user_id),
+	user_avatar text DEFAULT('guest.png'),
+	PRIMARY KEY (user_id)
 );
